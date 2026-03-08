bcfd1a72047b5a8bef5614d72e5dbf8b5ce47fdb
Ruslan Reshetniak <reshetnyakrm@gmail.com>
2026-03-08 22:14:40 +0100

    08.03.2026 rreshetniak: feat - Updated components to receive and use props instead of hardcoded values.

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -4,13 +4,20 @@ import TodoInfo from "../components/TodoInfo";
 import TodoList from "../components/TodoList";
 
 const Todo = () => {
+  const tasks = [
+    {id: 'task-1', title: 'Buy milk', isDone: false},
+    {id: 'task-2', title: 'Read book', isDone: true},
+  ];
   return (
     <div className="todo">
       <h1 className="todo__title">To Do List</h1>
       <AddTaskForm />
       <SearchTaskForm />
-      <TodoInfo />
-      <TodoList />
+      <TodoInfo 
+        total = {tasks.length}
+        done = {tasks.filter(({isDone}) => isDone).length}
+      />
+      <TodoList tasks={tasks}/>
     </div>
   );
 };
